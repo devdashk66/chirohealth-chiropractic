a1fec5b8db1bcfc13598ae4ed897da7060473f7b
devdashk66 <devdashk66@gmail.com>
2024-07-13 14:39:07 +0600

    home page done

--- src/components/home/ExpertsTherapist.jsx
+++ src/components/home/ExpertsTherapist.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   FaFacebook,
   FaInstagram,
@@ -20,19 +21,26 @@ const ExpertsTherapist = () => {
       <main className="flex flex-wrap items-center justify-center">
         {experts.map((expert) => (
           <div key={expert.id} className="flex flex-col group overflow-hidden">
-            <div
-              className={`bg-[url('/images/profile-images/${
-                "avater-" + expert.id
-              }.jpg')] bg-cover bg-center h-96 w-screen md:w-full min-w-80 py-10 text-2xl group-hover:scale-110 duration-200`}
-            >
-              <div className=" flex items-end justify-center h-full gap-3 mt-20 group-hover:mt-0 duration-200">
-                <FaTwitter className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
-                <FaFacebook className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
-                <FaInstagram className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
-                <FaWhatsapp className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
-                <FaTelegram className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
+            <div className="w-full md:max-w-[300px] h-96 relative">
+              <Image
+                src={`/images/profile-images/avater-${expert.id}.jpg`}
+                width={720}
+                height={600}
+                alt="avater"
+                className="w-full h-full object-cover"
+              />
+
+              <div className="absolute -bottom-12 left-2/4 -translate-x-2/4 -translate-y-2/4 group-hover:bottom-5 duration-300">
+                <div className="flex items-end justify-center h-full gap-3 z-30">
+                  <FaTwitter className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
+                  <FaFacebook className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
+                  <FaInstagram className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
+                  <FaWhatsapp className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
+                  <FaTelegram className="text-white p-2 w-8 h-8 bg-primary rounded-full cursor-pointer hover:bg-secondary duration-200" />
+                </div>
               </div>
             </div>
+
             <div className=" text-white bg-primary text-center py-3 relative z-20">
               <h2 className="text-xl font-bold">{expert.name}</h2>
               <p>{expert.position}</p>
@@ -53,3 +61,5 @@ const experts = [
   { id: 4, name: "Alicic Henderson", position: "Message Therapist" },
   { id: 5, name: "Devid Woa", position: "Desk Coordinator" },
 ];
+
+// className={`bg-[url('/images/profile-images/avater-${expert.id}.jpg')] bg-cover bg-center h-96 w-screen md:w-full min-w-80 py-10 text-2xl group-hover:scale-110 duration-200`}
