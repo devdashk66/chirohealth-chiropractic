b03df19a9e60d5b16d57aa15ab8e21094f971302
devdashk66 <devdashk66@gmail.com>
2024-07-13 20:07:46 +0600

    about page done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,15 @@
+import History from "@/components/about/History";
 import PageHeading from "@/components/common/PageHeading";
+import AboutSection from "@/components/home/AboutSection";
+import Testimonial from "@/components/home/Testimonial";
 
 const AboutPage = () => {
   return (
     <section>
       <PageHeading title="About Us" />
+      <AboutSection />
+      <History />
+      <Testimonial />
     </section>
   );
 };
---
+++ src/components/about/History.jsx
@@ -0,0 +1,21 @@
+const History = () => {
+  return (
+    <section className="py-12">
+      <main className="container mx-auto px-5 ">
+        <div className="max-w-[1000px]">
+          <h2 className="text-4xl text-gray-800 pb-5">ChiroHealth History</h2>
+          <p className="text-gray-600 text-lg">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            deserunt cumque temporibus, fugit doloremque placeat soluta quisquam
+            nam numquam itaque corrupti voluptatem provident labore autem
+            veritatis vero quod saepe illum optio voluptas! Cumque inventore
+            consequuntur vel aliquam minima eos. Labore corrupti recusandae ab
+            quae atque fuga dolores optio voluptatem tempora?
+          </p>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default History;
